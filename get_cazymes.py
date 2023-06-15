9045ef8283677958692f9eb05601d11f1f9f3f1b
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2023-06-15 14:07:34 -0600

    Create get_cazymes.py
    The script uses the output file "overview" of dbcan and an output file.
    It just enlists the results of dbcan keeping with a cazyme at leas founded with one algorithm.
    
    In case of more than one algorithm found the cazyme, the output name of eCAMI is kept
    If it just found the cazyme with DIAMOND and HMMER, DIAMOND name is kept.
    Finally it was just founded with HMMER, it is kept but deleting the position of the sequences
    #If the gene is composed of more than one cazyme, all the cazymes are kept and enlisted.

---
+++ get_cazymes.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 13:50:20 2023
+
+@author: pacomax
+"""
+import re
+import sys
+
+#The script uses the output file "overview" of dbcan and a output file.
+#It just enlists the results of dbcan keeping with a cazyme at leas founded
+#with one algorith.
+
+#In case of more than one algorith found the cazyme, the output name of eCAMI is keeped
+#If it just found the cazyme with DIAMOND and HMMER, DIAMOND name is keeped.
+#Finally it was just founded with HMMER, it is keeped but deleting the possition of the sequeces
+#If the gene is componned of more than one cazyme, all the cazymes are keeped and enlisted.
+
+Cazy_in = sys.argv[1]
+Cazy_ou = sys.argv[2]
+
+
+Cazy=[]
+with open(Cazy_in, "r") as file:
+    # Itera sobre cada línea del archivo
+    for line in file:
+        # Procesa la línea
+        line = line.strip()  # Elimina espacios en blanco y caracteres de nueva línea
+        variables = line.split("\t")  # Separa la línea en variables por tabulación
+        if variables[5]=="3":
+            Cazy.append(variables[3])
+        if variables[5]=="2":
+            if variables[3]!="-":
+                Cazy.append(variables[3])
+            else:
+                Cazy.append(variables[4])
+        if variables[5]=="1":
+            if variables[2]!="-":
+                Cazy.append(variables[2])
+            if variables[3]!="-":
+                Cazy.append(variables[3])
+            if variables[4]!="-":
+                Cazy.append(variables[4])
+        # Realiza alguna operación con las variables
+    for variable in Cazy:
+        variable=variable.split("+")
+        new_variable= [re.sub(r'\(\w+-\w+\)', '', item)  for item in  variable]
+        new_variable_2= [re.sub(r'_\w+', '', item_2)  for item_2 in  new_variable]
+        print(new_variable_2)
+        with open(Cazy_ou, "a") as file:
+            # Write each item from the list to a new line in the file
+            file.write("\n".join(new_variable_2)+"\n")
